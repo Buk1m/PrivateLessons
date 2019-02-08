16757a7fb7cabe1c8dedf3eb536b8affc06e5f0e
Creiziol <amoczek2006@gmail.com>
2019-02-08 18:20:57 +0100

    Zestaw powtórzeniowy

--- Exercise1/ZestawPowtorzeniowy1.cs
+++ Exercise1/ZestawPowtorzeniowy1.cs
@@ -11,7 +11,16 @@ namespace Exercise1
         // Oblicz sume elementów w tablicy 'int[] tab' za pomocą dowolnej pętli
         public int Cwiczenie1(int[] tab)
         {
-            return Exit;
+
+            int sum = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                sum = tab[i] + sum;
+
+
+
+            }
+            return sum;
         }
 
         // dla kazdego elementu w tablicy 'int[] tab' sprawdz czy jest on liczba
@@ -22,16 +31,36 @@ namespace Exercise1
         //zwróc tablice [false,true,false]
         public bool[] Cwiczenie2(int[] tab)
         {
-            return null;
+            bool[] nerf = new bool[tab.Length];
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] % 2 == 0 && tab[i] % 3 == 0)
+                {
+                    nerf[i] = true;
+                }
+            }
+            return nerf;
         }
 
         //Napisz funkcje ktora przyjmuje jako parametr napis i pojedyncza litere
         //po czym oblicza ile razy ta litera wystpępuje w napisie i zwraca tę wartość
         //Wskazówka:
-        //napis typu string to nic innego jak tablica znakow char[], wykorzystaj petle i warunek if :)
+        //napis typu string to nic innego jak tablica znakow char[], wykorzystaj petle i warunek if :|\|
         public int Cwiczenie3(string napis, char litera)
         {
-            return Exit;
+            int p = 0;
+            for (int i = 0; i < napis.Length; i++)
+            {
+                if (napis[i] == litera)
+                {
+
+                    p++;
+                }
+
+
+            }
+            return p;
         }
 
         //Trudne
@@ -39,7 +68,24 @@ namespace Exercise1
         //wykorzystaj funkcje, która napisales w Cwiczeniu3 :)
         public char Cwiczenie4(string napis)
         {
-            return ' ';
+            int p = 0;
+            char r = ' ';
+            // Cwiczenie3(napis,)
+            for (int i = 0; i < napis.Length; i++)
+            {
+
+                int result = Cwiczenie3(napis, napis[i]);
+
+                if (p < result)
+                {
+                    r = napis[i];
+                    p = result;
+                }
+
+            }
+
+
+            return r;
         }
 
 
@@ -47,9 +93,22 @@ namespace Exercise1
 
         // Połowa
         // Dla podanego napisu zwroc jego polowę
+
+        // Przyklad:
+        // napis = "Overwatch"
+        // zwroc "Over"
         public string Cwiczenie5(string napis)
         {
-            return null;
+
+            int dr = napis.Length / 2;
+            string pol = "";
+            for (int i = 0; i < dr; i++)
+            {
+                pol = pol + napis[i];
+
+            }
+
+            return pol;
         }
 
         //wpisz wszystkie elementy tablicy
@@ -57,7 +116,12 @@ namespace Exercise1
         //Do wypisywania uzyj Console.Write(); zamiast Console.WriteLine();
         public void Cwiczenie6(int[] tab)
         {
-        
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write(tab[i]);
+
+            }
+
         }
 
         //wypisz napis podany jako parametr, n razy
@@ -65,7 +129,11 @@ namespace Exercise1
         //Do wypisywania uzyj Console.Write(); zamiast Console.WriteLine();
         public void Cwiczenie7(string napis, int n)
         {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(napis);
 
+            }
         }
 
 
@@ -81,10 +149,28 @@ namespace Exercise1
 
         }
 
-        //Policz tylko parzyste elementy w tablicy 'int[] tab' i zwroc wynik
+        //Policz tylko parzyste elementy w tablicy 'int[] tab' i zwroc wynik\
+        //przyklad:
+        //tab = [1,2,3,4]
+        //zwroc 2+4 czyli 6
+
         public int Cwiczenie9(int[] tab)
         {
-            return Exit;
+            int dodawanie = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] % 2 == 0)
+                {
+                    dodawanie = tab[i] + dodawanie;
+
+                }
+
+
+            }
+
+
+
+            return dodawanie;
         }
     }
 }
